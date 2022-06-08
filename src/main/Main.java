505c3a43e755723e7eb3bf4f0cf7b6031f878a1b
Jordan Perez <74106613+JapaJordan@users.noreply.github.com>
2022-06-08 16:22:52 -0300

    interfazGraficaLogin_Lista

---
+++ src/main/Main.java
@@ -0,0 +1,12 @@
+
+package main;
+
+import views.LoginView;
+
+public class Main {
+    public static void main(String[] args) {
+        //Instancia de Login
+        LoginView login = new LoginView();
+        login.setVisible(true); 
+    }
+}
