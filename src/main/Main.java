437a47b0937439bfe6dabf52461c86eb66d1dfb5
Jordan Perez <palfonsojordan@gmail.com>
2022-06-08 20:45:56 -0300

    inicio_SystemView

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,11 +2,14 @@
 package main;
 
 import views.LoginView;
+import views.SystemView;
 
 public class Main {
     public static void main(String[] args) {
         //Instancia de Login
-        LoginView login = new LoginView();
-        login.setVisible(true); 
+       LoginView login = new LoginView();
+       login.setVisible(true); 
+        //SystemView viewAdmin = new SystemView();
+        //viewAdmin.setVisible(true);
     }
 }
