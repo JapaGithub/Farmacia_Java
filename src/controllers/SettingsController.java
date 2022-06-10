e5749fcca810170306d12fdaf29534fec2f6fa53
Jordan Perez <palfonsojordan@gmail.com>
2022-06-10 10:29:15 -0300

    menu_Mouse_Hover_Listo

---
+++ src/controllers/SettingsController.java
@@ -0,0 +1,79 @@
+package controllers;
+
+import java.awt.Color;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import views.SystemView;
+
+public class SettingsController implements MouseListener {
+
+    private SystemView views; //Instanciar la vista
+
+    public SettingsController(SystemView views) { //Constructor q recibe como parametro views de clase objeto SystemView
+        this.views = views;
+        //Vamos a poner los panels y labels en escucha los colocamos en el constructor
+        this.views.jLabelProducts.addMouseListener(this);
+        this.views.jLabelPurchases.addMouseListener(this);
+        this.views.jLabelCustomers.addMouseListener(this);
+        this.views.jLabelEmployees.addMouseListener(this);
+        this.views.jLabelSuppliers.addMouseListener(this);
+        this.views.jLabelCategories.addMouseListener(this);
+        this.views.jLabelReports.addMouseListener(this);
+        this.views.jLabelSettings.addMouseListener(this);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        if (e.getSource() == views.jLabelProducts) {
+            views.jPanelProducts.setBackground(new Color(0,153,153));
+        } else if (e.getSource() == views.jLabelPurchases) {
+            views.jPanelPurchases.setBackground(new Color(0,153,153));
+        } else if (e.getSource() == views.jLabelCustomers) {
+            views.jPanelCustomers.setBackground(new Color(0,153,153));
+        } else if (e.getSource() == views.jLabelEmployees) {
+            views.jPanelEmployees.setBackground(new Color(0,153,153));
+        } else if (e.getSource() == views.jLabelSuppliers) {
+            views.jPanelSuppliers.setBackground(new Color(0,153,153));
+        } else if (e.getSource() == views.jLabelCategories) {
+            views.jPanelCategories.setBackground(new Color(0,153,153));
+        } else if (e.getSource() == views.jLabelReports) {
+            views.jPanelReports.setBackground(new Color(0,153,153));
+        } else if (e.getSource() == views.jLabelSettings) {
+            views.jPanelSettings.setBackground(new Color(0,153,153));
+        }
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        if (e.getSource() == views.jLabelProducts) {
+            views.jPanelProducts.setBackground(new Color(18, 45, 61));
+        } else if (e.getSource() == views.jLabelPurchases) {
+            views.jPanelPurchases.setBackground(new Color(18, 45, 61));
+        } else if (e.getSource() == views.jLabelCustomers) {
+            views.jPanelCustomers.setBackground(new Color(18, 45, 61));
+        } else if (e.getSource() == views.jLabelEmployees) {
+            views.jPanelEmployees.setBackground(new Color(18, 45, 61));
+        } else if (e.getSource() == views.jLabelSuppliers) {
+            views.jPanelSuppliers.setBackground(new Color(18, 45, 61));
+        } else if (e.getSource() == views.jLabelCategories) {
+            views.jPanelCategories.setBackground(new Color(18, 45, 61));
+        } else if (e.getSource() == views.jLabelReports) {
+            views.jPanelReports.setBackground(new Color(18, 45, 61));
+        } else if (e.getSource() == views.jLabelSettings) {
+            views.jPanelSettings.setBackground(new Color(18, 45, 61));
+        }   
+    }
+
+}
