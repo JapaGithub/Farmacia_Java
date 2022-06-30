39174c77376d3b7bfbaee5063db1351aa8550705
Jordan Perez <palfonsojordan@gmail.com>
2022-06-30 14:55:00 -0300

    registerEmployeeQuery_and_listEmployeesQuery
    Registrar y listar todos los empleados, asi como tambien query para el boton de search

--- src/models/EmployeesDao.java
+++ src/models/EmployeesDao.java
@@ -1,9 +1,13 @@
 package models;
 
+import java.sql.Timestamp;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;//es el conjunto de datos que obtenemos al enviar la consulta
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import javax.swing.JOptionPane;
 
 public class EmployeesDao {
@@ -72,5 +76,70 @@ public class EmployeesDao {
         }
         
         return employee;
+    }
+    
+    
+    public boolean registerEmployeeQuery(Employees employee){
+         String query = "INSER INTO employees(id, full_name, username, address, telephone, email "
+                 + "password, rol, created, updated) VALUES (?,?,?,?,?,?,?,?,?,?)";
+         
+         Timestamp datetime = new Timestamp(new Date().getTime());//sirve para tener la fecha exacta, lo proporciona Java
+         
+         try{
+             conn = cn.getConnection();
+             pst = conn.prepareStatement(query);
+             //acceder a los metodos Setter del empleado para enviar los datos q se registraran en la DB
+             pst.setInt(1, employee.getId());
+             pst.setString(2, employee.getFull_name());
+             pst.setString(3, employee.getUsername());
+             pst.setString(4, employee.getAddress());
+             pst.setString(5, employee.getTelephone());
+             pst.setString(6, employee.getEmail());
+             pst.setString(7, employee.getPassword());
+             pst.setString(8, employee.getRol());
+             //para created y updated se pasara la variable datetime para que traiga la fecha y hora exacta del registro y de la actualizacion
+             pst.setTimestamp(9, datetime);
+             pst.setTimestamp(10, datetime);
+             pst.execute();//ejecutamos la sentencia sql (variable query)
+             return true;
+             
+         }catch(SQLException e){
+             JOptionPane.showMessageDialog(null, "Error al registrar el usuario "+ e);
+             return false;
+         }
+    }
+
+    public List listEmployeesQuery(String value){
+     //metodo que retorna una lista, value va funcionar para el buscador
+        List<Employees> list_employees = new ArrayList();
+        String query = "SELECT * FROM employees ORDER By rol ASC";//para traer todos los empleados
+        String query_search_employee = "SELECT * FROM employees WHERE id LIKE '%" + value + "%'";//para traer el resultado de la busqueda
+        
+        try{
+            conn = cn.getConnection();
+            if(value.equalsIgnoreCase("")){
+                pst = conn.prepareStatement(query);
+                rs = pst.executeQuery();
+            }else{
+                pst = conn.prepareStatement(query_search_employee);
+                rs = pst.executeQuery();
+            }
+            
+            //para recorrer la lista de empleados
+            while(rs.next()){
+                Employees employee = new Employees();
+                employee.setId(rs.getInt("id"));
+                employee.setFull_name(rs.getString("full_name"));
+                employee.setUsername(rs.getString("username"));
+                employee.setAddress(rs.getString("address"));
+                employee.setTelephone(rs.getString("telephone"));
+                employee.setEmail(rs.getString("email"));
+                employee.setRol(rs.getString("rol"));
+                list_employees.add(employee);//pasar toda la informacion a la lista
+            }      
+        }catch(SQLException e){
+            JOptionPane.showMessageDialog(null, e.toString());
+        }
+        return list_employees;
     }
 }
